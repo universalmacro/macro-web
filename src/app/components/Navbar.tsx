b38bc35ef9997b838639e68dd4a8e156c35cda43
jaythan <yegarrick12@gmail.com>
2024-01-31 11:13:47 +0800

    logo

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -32,9 +32,9 @@ const Navbar = () => {
       <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
         <Link
           href={"/"}
-          className="text-2xl md:text-5xl text-white font-semibold"
+          className="text-xl md:text-2xl text-white font-semibold"
         >
-          LOGO
+          <img src="/images/logo_long.png" className="h-[50px]" />
         </Link>
         <div className="mobile-menu block md:hidden">
           {!navbarOpen ? (
