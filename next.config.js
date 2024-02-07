4c4ad995c42ed5bc282ad82e0bcb5b230f86fa36
jaythan <yegarrick12@gmail.com>
2024-02-07 14:48:55 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["static-1318552943.cos.ap-singapore.myqcloud.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "static-1318552943.cos.ap-singapore.myqcloud.com",
+        port: "",
+        pathname: "/macro/**",
+      },
+    ],
   },
 };
 
